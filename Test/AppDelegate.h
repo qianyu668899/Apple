f92f14237ac7506d56b3b6b724e68f8ed87e7146
qianyu668899 <jinggeqianyu1991@gmail.com>
2015-04-22 15:27:28 -0400

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Yu QIAN on 2015-04-22.
+//  Copyright (c) 2015 Yu QIAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Yu QIAN on 2015-04-22.
+//  Copyright (c) 2015 Yu QIAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
